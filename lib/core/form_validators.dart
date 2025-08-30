f7f92e9cfa1c7fa506a34db10ac9cba33b7fe742
Abdul Rahman <abdulrahmanmlg744@gmail.com>
2025-08-30 12:42:45 +0530

    FEAT(FormValidator):ADD Form Validatorsn

---
+++ lib/core/form_validators.dart
@@ -0,0 +1,30 @@
+class FormValidators {
+  static String? requiredField(String? value, {String? fieldName = "Field"}) {
+    if (value == null || value.trim().isEmpty) {
+      return "$fieldName is required";
+    }
+    return null;
+  }
+
+  static String? email(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return "Email is Required";
+    }
+
+    if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+        .hasMatch(value)) {
+      return "Enter Valid Email";
+    }
+    return null;
+  }
+
+  static String? password(String? value, {int minLength = 6}) {
+    if (value == null || value.trim().isEmpty) {
+      return "Password is requred";
+    }
+    if (value.length < minLength) {
+      return "Passsword must be atleast $minLength characters";
+    }
+    return null;
+  }
+}
